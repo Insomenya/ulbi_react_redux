15f048c7ca65b912db46bd4064c5343f118181a0
Insomenya <kolowertnov.alexej@yandex.com>
2025-02-17 01:52:17 +0400

    added search functionality

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useGetReposQuery } from "../../services/repoService";
 import Repo from "./repo/repo";
+import './main.less';
 
 const Main = (props) => {
-    const { data, error, isFetching } = useGetReposQuery('query');
+    const [inputValue, setInputValue] = useState('');
+    const [searchValue, setSearchValue] = useState('');
+    const { data = { items: [] }, isFetching, isUninitialized } = useGetReposQuery(searchValue);
+
+    function searchHandler() {
+        setSearchValue(inputValue);
+    }
 
     return (
         <div>
-            {!isFetching && data.items.map((repo) => (
+            <div className="search">
+                <input type="text" placeholder="Input repo name" className="search-input" value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
+                <button className="search-btn" onClick={() => searchHandler()} >Search</button>
+            </div>
+            {(isUninitialized || isFetching) ? <div className="fetching"></div> :
+            data.items.map((repo) => (
                 <Repo repo={repo} key={repo.id}></Repo>
             ))}
         </div>
